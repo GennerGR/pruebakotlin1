43e7fee54ea3bc441a7d166acbb871f770162ed0
Genner <lopezitzepcgenneralexander@gmail.com>
2024-08-27 15:41:14 -0600

    Navegación Básica con Voyager

--- composeApp/src/commonMain/kotlin/com/gnr/kotlinmultiplatform/App.kt
+++ composeApp/src/commonMain/kotlin/com/gnr/kotlinmultiplatform/App.kt
@@ -1,39 +1,66 @@
 package com.gnr.kotlinmultiplatform
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import pruebakotlin1.composeapp.generated.resources.Res
-import pruebakotlin1.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
+        Navigator(screen = MainScreen())
+    }
+}
+
+class MainScreen : Screen {
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+            Button(onClick = {
+                navigator.push(SecondScreen())
+            }) {
+                Text("Navegación básica", fontSize = 24.sp, color = Color.White)
             }
         }
     }
 }
 
+class SecondScreen : Screen {
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
+        Column(
+            modifier = Modifier.fillMaxSize().background(Color.Blue),
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            Text("SECOND SCREEN", fontSize = 32.sp, color = Color.White)
+            //Spacer(Modifier.padding(16.dp))
+            Button(onClick = {
+                navigator.push(MainScreen())
+            }) {
+                Text("Back")
+            }
+        }
+    }
+
+}
 
